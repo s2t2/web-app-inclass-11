9c97b2ffdfe5d7d06901518c73c51d0dc99e9dd6
Mike Rossetti <s2t2@users.noreply.github.com>
2020-02-24 12:52:12 -0500

    Render JSON data

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 
 
 
+# app.py
 
-
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -15,3 +15,13 @@ def index():
 @app.route("/about")
 def about():
     return "About me"
+
+@app.route("/books")
+@app.route("/books.json")
+def list_books():
+    books = [
+        {"id": 1, "title": "Book 1"},
+        {"id": 2, "title": "Book 2"},
+        {"id": 3, "title": "Book 3"},
+    ]
+    return jsonify(books)
