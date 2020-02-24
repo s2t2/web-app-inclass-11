7546c63f2a9ce14c15375066aab92e31245e71e9
Mike Rossetti <s2t2@users.noreply.github.com>
2020-02-24 14:03:15 -0500

    Configured and migrated a sqlite database

--- web_app/app.py
+++ web_app/app.py
@@ -2,6 +2,8 @@
 # app.py
 
 from flask import Flask, jsonify, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
@@ -10,6 +12,21 @@ def create_app():
 
     app = Flask(__name__)
 
+    #
+    # configuring the database
+    #
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///web_app_11.db"
+    db = SQLAlchemy(app)
+    migrate = Migrate(app, db)
+
+    class Book(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        title = db.Column(db.String(128))
+        author_id = db.Column(db.String(128))
+
+    #
+    # registering routes:
+    #
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
 
