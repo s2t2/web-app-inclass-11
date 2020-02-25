3d28062725efa1403b6152d230117a635eb83544
Mike Rossetti <s2t2@users.noreply.github.com>
2020-02-25 12:43:33 -0500

    Refactor basilica service to facilitate imports from other files

--- web_app/basilica_service.py
+++ web_app/basilica_service.py
@@ -11,11 +11,13 @@ API_KEY = os.getenv("BASILICA_API_KEY")
 
 connection = basilica.Connection(API_KEY)
 
-sentences = ["Hello world!", "How are you?"]
-embeddings = connection.embed_sentences(sentences)
-print(type(embeddings))
+if __name__ == "__main__":
 
-for embedding in embeddings:
-    print(len(embedding)) #> 768
-    print(list(embedding)) # [[0.8556405305862427, ...], ...]
-    print("-------------")
+    sentences = ["Hello world!", "How are you?"]
+    embeddings = connection.embed_sentences(sentences)
+    print(type(embeddings))
+
+    for embedding in embeddings:
+        print(len(embedding)) #> 768
+        print(list(embedding)) # [[0.8556405305862427, ...], ...]
+        print("-------------")
