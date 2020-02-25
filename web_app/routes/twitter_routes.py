782fb33d0e45a301b124d01c52702dd69e7e2e7e
Mike Rossetti <s2t2@users.noreply.github.com>
2020-02-25 13:28:38 -0500

    Integrate twitter api into routing; display user info on page

--- web_app/routes/twitter_routes.py
+++ web_app/routes/twitter_routes.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, render_template
 
+from web_app.twitter_service import twitter_api
+
+twitter_api_client = twitter_api()
+
 twitter_routes = Blueprint("twitter_routes", __name__)
 
 @twitter_routes.route("/users/<screen_name>")
 def get_user(screen_name=None):
     print(screen_name)
-    return f"TODO GET TWEETS AND INFO FOR {screen_name}"
+    twitter_user = twitter_api_client.get_user(screen_name)
+    return render_template("user.html", user=twitter_user)
--- web_app/twitter_service.py
+++ web_app/twitter_service.py
@@ -10,30 +10,34 @@ TWITTER_API_SECRET = os.getenv("TWITTER_API_SECRET")
 TWITTER_ACCESS_TOKEN = os.getenv("TWITTER_ACCESS_TOKEN")
 TWITTER_ACCESS_TOKEN_SECRET = os.getenv("TWITTER_ACCESS_TOKEN_SECRET")
 
-auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
-print("AUTH", auth)
-auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+def twitter_api():
+    auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
+    auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
+    print("AUTH", auth)
+    api = tweepy.API(auth)
+    print("API", api)
+    #print(dir(api))
+    return api
 
-api = tweepy.API(auth)
-print("API", api)
-#print(dir(api))
+if __name__ == "__main__":
 
-user = api.get_user("Chrisalbon")
-print("USER", user)
-print(user.screen_name)
-print(user.name)
-print(user.followers_count)
+    api = twitter_api()
+    user = api.get_user("Chrisalbon")
+    print("USER", user)
+    print(user.screen_name)
+    print(user.name)
+    print(user.followers_count)
 
-#breakpoint()
-
+    #breakpoint()
 
 
 
 
-#public_tweets = api.home_timeline()
-#
-#for tweet in public_tweets:
-#    print(type(tweet)) #> <class 'tweepy.models.Status'>
-#    #print(dir(tweet))
-#    print(tweet.text)
-#    print("-------------")
+
+    #public_tweets = api.home_timeline()
+    #
+    #for tweet in public_tweets:
+    #    print(type(tweet)) #> <class 'tweepy.models.Status'>
+    #    #print(dir(tweet))
+    #    print(tweet.text)
+    #    print("-------------")
