bc278140c18064bfb8c64e912c577ab593b8d9be
Mike Rossetti <s2t2@users.noreply.github.com>
2020-02-26 13:19:27 -0500

    Fetching tweets from the db

--- web_app/routes/stats_routes.py
+++ web_app/routes/stats_routes.py
@@ -4,6 +4,8 @@ from flask import Blueprint, request, jsonify
 from sklearn.datasets import load_iris
 from sklearn.linear_model import LogisticRegression
 
+from web_app.models import User, Tweet
+
 stats_routes = Blueprint("stats_routes", __name__)
 
 @stats_routes.route("/iris")
@@ -22,4 +24,23 @@ def iris():
 def predict():
     print("PREDICTING...")
     print("FORM DATA:", dict(request.form))
+    #> {'screen_name_a': 'elonmusk', 'screen_name_b': 'chrisalbon', 'tweet_text': 'Example tweet text here'}
+    screen_name_a = request.form["screen_name_a"]
+    screen_name_b = request.form["screen_name_b"]
+    tweet_text = request.form["tweet_text"]
+
+    print("FETCHING TWEETS FROM THE DATABASE...")
+    # todo: wrap in a try block in case the user's don't exist in the database
+    user_a = User.query.filter(User.screen_name == screen_name_a).one()
+    user_b = User.query.filter(User.screen_name == screen_name_b).one()
+
+    user_a_tweets = user_a.tweets
+    user_b_tweets = user_b.tweets
+
+    user_a_embeddings = [tweet.embedding for tweet in user_a_tweets]
+    user_b_embeddings = [tweet.embedding for tweet in user_b_tweets]
+
+
+    print("TRAINING THE MODEL...")
+
     return jsonify({"message": "RESULTS (TODO)"})
